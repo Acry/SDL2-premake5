cf672a0c1dd1b9e566c0ae8955619515a4df4f68
Carsten Holtkamp <carsten-holtkamp@t-online.de>
2018-05-05 19:14:50 +0200

    premake5 clean - now clean builddir cherry pick would be = make clean

--- premake5.lua
+++ premake5.lua
@@ -27,34 +27,49 @@ project "SDL2_Test"
 	      "-lSDL2_image",
 	      "-lm"
       }
-      -- windows library cflags and libs
---       configuration { "windows" }
---       includedirs { "SDL/include" }
---       libdirs { "../RecastDemo/Contrib/SDL/lib/x86" }
---       debugdir "../RecastDemo/Bin/"
---       links { 
--- 	      "glu32",
--- 	      "opengl32",
--- 	      "SDL2",
--- 	      "SDL2main",
---       }
-      -- mac includes and libs
---       configuration "macosx"
---       links {"OpenGL.framework","CoreFoundation.framework","SDL2.framework"}
---       
---       configuration {"macosx", "gmake"}
---       buildoptions {"-F /Library/Frameworks", "-F ~/Library/Frameworks"}
---       linkoptions {"-F /Library/Frameworks", "-F ~/Library/Frameworks"}
 
---       configuration { "macosx" }
---       kind "ConsoleApp"
---       includedirs { "/Library/Frameworks/SDL2.framework/Headers" }
---       buildoptions { "-Wunused-value -Wshadow -Wreorder -Wsign-compare -Wall" }
---       links { 
--- 	      "SDL2.framework"
---       }
+      
+      newaction
+      {
+	      trigger     = "clean",
+	      
+	      description = "Clean all build files and output",
+	      
+	      execute = function ()
+		      
+		      files_to_delete = 
+		      {
+			      "Makefile",
+			      "*.make"
+		      }
+		      
+		      directories_to_delete = 
+		      {
+			      "obj",
+			      "bin",
+		      }
+		      
+		      for i,v in ipairs( directories_to_delete ) do
+			      os.rmdir( v )
+		      end
+		      
+		      if not os.is "windows" then
+			      os.execute "find . -name .DS_Store -delete"
+			      for i,v in ipairs( files_to_delete ) do
+				      os.execute( "rm -f " .. v )
+			      end
+		      else
+			      for i,v in ipairs( files_to_delete ) do
+				      os.execute( "del /F /Q  " .. v )
+			      end
+		      end
+		      
+	      end
+      }
+      
 	postbuildcommands {
 		-- copy assets
 		"{MKDIR} bin/%{cfg.buildcfg}/assets/gfx/",
 		"{COPY} assets/gfx/* bin/%{cfg.buildcfg}/assets/gfx"
 	}
+	
